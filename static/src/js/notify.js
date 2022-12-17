401a37430a69770747e69973dcf8a788edacfccd
Demelash <demelash@Ubuntu.myguest.virtualbox.org>
2022-12-17 10:34:54 +0300

    adding javascript notification

--- static/src/js/notify.js
+++ static/src/js/notify.js
@@ -8,6 +8,8 @@ odoo.define('odoo-js.NotifyWidget', function (require) {
     saveRecord: function () {
       var res = this._super.apply(this, arguments);
       this.do_notify('success', 'Record Saved');
+         const audio = new Audio("/odoo-js/static/src/sound/success.wav");
+         audio.play();
       return res;
 
     },
