870e021c387f045c8bd0090e4915803b3e3f9326
Demelash <demelash@Ubuntu.myguest.virtualbox.org>
2022-12-17 09:51:24 +0300

    __manifest__.py

---
+++ __manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Odoo JS",
+    "summary": "Odoo JavaScript",
+    "version": "0.1.0",
+    "category": "Services",
+    "author": "Demelash Kasaye",
+    "website": "https://demelashkasaye.com",
+    "application": True,
+    'sequence': -10,
+    "depends": [
+        "base",
+
+    ],
+    "data": [
+        "views/assets.xml",
+    ]
+}
