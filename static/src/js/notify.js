e0946e7069e35fc64b553a56e812e6d62edac7a7
Demelash <demelash@Ubuntu.myguest.virtualbox.org>
2022-12-19 15:57:36 +0300

    Modify Module Name
    Change module name from odoo-js  to odoo_js to avoid conflict

--- static/src/js/notify.js
+++ static/src/js/notify.js
@@ -1,4 +1,4 @@
-odoo.define('odoo-js.NotifyWidget', function (require) {
+odoo.define('odoo_js.NotifyWidget', function (require) {
     "use strict";
     console.log('LOADING...')
 
@@ -8,7 +8,7 @@ odoo.define('odoo-js.NotifyWidget', function (require) {
     saveRecord: function () {
       var res = this._super.apply(this, arguments);
       this.do_notify('success', 'Record Saved');
-         const audio = new Audio("/odoo-js/static/src/sound/success.wav");
+         const audio = new Audio("/odoo_js/static/src/sound/success.wav");
          audio.play();
       return res;
 
